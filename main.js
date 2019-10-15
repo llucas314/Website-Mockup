9ae7bf375273b1b72f28c032c2510c78dcd89d75
Ren Lucas <llucas314@gmail.com>
2019-10-15 11:28:06 -0400

    added main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.menu-wrapper');
 const menu = document.querySelector('div.menu');
+menu.style.visibility = "hidden"
 hamburger.addEventListener('click', function(){
     if (menu.style.visibility === "hidden"){
     menu.style.visibility = "visible"
